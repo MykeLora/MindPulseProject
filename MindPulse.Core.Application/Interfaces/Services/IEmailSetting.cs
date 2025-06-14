8cb065cabbcc552735d3d022375c1083929d0fd6
MykeLora <MykeLora@gmail.com>
2025-06-14 15:43:58 -0400

    Entity Email added whit some change

---
+++ MindPulse.Core.Application/Interfaces/Services/IEmailSetting.cs
@@ -0,0 +1,15 @@
+﻿using MindPulse.Core.Domain.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindPulse.Core.Application.Interfaces.Services
+{
+    public interface IEmailService
+    {
+        public EmailSettings EmailSettings { get; }
+        Task SendAsync(EmailRequest emailRequest);
+    }
+}

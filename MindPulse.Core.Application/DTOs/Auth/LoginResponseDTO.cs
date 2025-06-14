8c94c497349213c9a6429a617a6c93314f902242
MykeLora <MykeLora@gmail.com>
2025-06-14 11:35:05 -0400

    DTOs user created

---
+++ MindPulse.Core.Application/DTOs/Auth/LoginResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindPulse.Core.Application.DTOs.Auth
+{
+    public class LoginResponseDTO
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Message { get; set; }
+    }
+}

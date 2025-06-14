faac1ec4f2dd2ea85480eace60853ac9ee28acd9
MykeLora <MykeLora@gmail.com>
2025-06-14 11:35:42 -0400

    Helper Hanspassword created

---
+++ MindPulse.Core.Application/Interfaces/Services/IPassWordHelperService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindPulse.Core.Application.Interfaces.Services
+{
+    public interface IPassWordHelperService
+    {
+        string HashPassword(string password);
+        string GenerateTemporalPassword(int longitud = 10);
+    }
+}

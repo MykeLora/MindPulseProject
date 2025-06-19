b29afd0afdd093cc802b79556469495d258b3a70
MykeLora <MykeLora@gmail.com>
2025-06-19 13:19:05 -0400

    ok

--- MindPulse.WebApp/Program.cs
+++ MindPulse.WebApp/Program.cs
@@ -5,27 +5,34 @@ using Microsoft.IdentityModel.Tokens;
 using MindPulse.Core.Application.Interfaces.Repositories;
 using MindPulse.Core.Application.Interfaces.Services;
 using MindPulse.Core.Application.Mappings;
-using MindPulse.Core.Application.Services.AuthService;
 using MindPulse.Core.Domain.Settings;
 using MindPulse.Infrastructure.Persistence.Context;
 using MindPulse.Infrastructure.Persistence.Repositories;
 using MindPulse.Infrastructure.Services;
+using MindPulse.Infrastructure.Shared;
 using MindPulse.Infrastructure.Shared.Services;
+using MindPulse.WebApp;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<ApplicationContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Registrar HttpContextAccessor
 builder.Services.AddHttpContextAccessor();
+
+// Registrar AutoMapper con el perfil DefaultProfile
 builder.Services.AddAutoMapper(typeof(DefaultProfile));
 
-
+// Registrar servicios específicos manualmente
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IEmailService, EmailService>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+
+builder.Services.AddControllers();
 
 
 builder.Services.AddControllers();
